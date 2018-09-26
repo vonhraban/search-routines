c8ecc5668fc1761f387c3f24b3f19d1143dd015a
Vlad <hraban@vivec.co.uk>
2018-09-26 19:01:24 +0100

    Simplify tests by making use of setUp unittest method

--- tests/test_haystack.py
+++ tests/test_haystack.py
@@ -4,31 +4,25 @@ from search.haystack import HaystackCollection, Operator
 
 class TestStringSearch(unittest.TestCase):
 
-    def test_find_containing_all_strings(self):
+    def setUp(self):
         input_strings = [
             'this is some test string',
             'this is another test string',
             'this is yet another test string'
         ]
-        haystacks = HaystackCollection(input_strings)
+        self.haystacks = HaystackCollection(input_strings)
 
+    def test_find_containing_all_strings(self):
         self.assertEqual(
-            haystacks.search(Operator('AND'), ['another', 'test']),
+            self.haystacks.search(Operator('AND'), ['another', 'test']),
             [1, 2]
         )
 
         self.assertEqual(
-            haystacks.search(Operator('AND'), ['yet', 'another']),
+            self.haystacks.search(Operator('AND'), ['yet', 'another']),
             [2]
         )
 
     def test_find_containing_any_strings(self):
-        input_strings = [
-            'this is some test string',
-            'this is another test string',
-            'this is yet another test string'
-        ]
-        haystacks = HaystackCollection(input_strings)
-
-        result = haystacks.search(Operator('OR'), ['yet', 'another'])
+        result = self.haystacks.search(Operator('OR'), ['yet', 'another'])
         self.assertEqual(result, [1, 2])
